4af14018ebf3820869386cff4dce50932f630b7e
Guilhem Desarcy-Lemiere <63927791+KoroKira@users.noreply.github.com>
2024-01-24 10:24:35 +0100

    Add files via upload

--- python/test.py
+++ python/test.py
@@ -1,27 +1,59 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.optimizers import Adam
 
-# Charger le modèle pré-entrainé GPT-3.5
-model = GPT2LMHeadModel.from_pretrained("EleutherAI/gpt-neo-2.7B")
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+# Chargement de vos données (supposons que vos données sont dans une liste de textes)
+# Assurez-vous que vos données sont prétraitées en fonction de vos besoins
+# Exemple: data = ["texte1", "texte2", ...]
+data = [...]  # Vos données ici
 
-def generer_reponse(entrée_utilisateur, model, tokenizer):
-    # Prétraiter l'entrée utilisateur
-    entree_encodée = tokenizer.encode(entrée_utilisateur, return_tensors="pt")
+# Création d'un vocabulaire à partir de vos données
+tokenizer = tf.keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(data)
+total_words = len(tokenizer.word_index) + 1
 
-    # Générer une réponse en utilisant le modèle
-    sortie = model.generate(entree_encodée, max_length=150, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
+# Préparation des séquences d'entraînement
+input_sequences = []
+for line in data:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
 
-    # Décode la sortie en texte
-    reponse = tokenizer.decode(sortie[0], skip_special_tokens=True)
+max_sequence_length = max([len(x) for x in input_sequences])
+input_sequences = tf.keras.preprocessing.sequence.pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
+X, y = input_sequences[:,:-1],input_sequences[:,-1]
+y = tf.keras.utils.to_categorical(y, num_classes=total_words)
 
-    return reponse
+# Définition du modèle
+model = Sequential()
+model.add(Embedding(total_words, 50, input_length=max_sequence_length-1))
+model.add(LSTM(100))
+model.add(Dense(total_words, activation='softmax'))
 
-# Boucle de dialogue
-while True:
-    entree_utilisateur = input("Vous: ")
+# Compilation du modèle
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    # Générer la réponse
-    reponse = generer_reponse(entree_utilisateur, model, tokenizer)
+# Entraînement du modèle
+model.fit(X, y, epochs=50, verbose=1)
 
-    # Afficher la réponse
-    print("IA:", reponse)
+# Fonction pour générer du texte à partir du modèle entraîné
+def generate_text(seed_text, next_words, model, max_sequence_length):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = tf.keras.preprocessing.sequence.pad_sequences([token_list], maxlen=max_sequence_length-1, padding='pre')
+        predicted = model.predict_classes(token_list, verbose=0)
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    return seed_text
+
+# Exemple d'utilisation
+seed_text = "Votre phrase de départ ici"
+generated_text = generate_text(seed_text, 10, model, max_sequence_length)
+print(generated_text)
